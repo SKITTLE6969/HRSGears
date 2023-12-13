dfe6da5eccb8a604032c3f0cd655080fd6cb5136
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2023-12-13 15:59:36 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,15 @@
-fx_version 'adamant'
-games {'gta5'}
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
 
 author 'Hugo Rafael Simoes'
 description 'CaixaManual'
 version '1.0'
 
+shared_script '@ox_lib/init.lua'
 
 client_scripts {
 	"client.lua",
 	"config.lua",	
-
 }
-
-
